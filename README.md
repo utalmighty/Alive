6d0f1cc35316a13c7ee326c54b6220f93317c2aa
Utkarsh Jaiswal <62197198+utalmighty@users.noreply.github.com>
2023-07-26 07:49:56 +0000

    Added Readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Environment Variables
+- EMAIL ```Sender email address(gmail)```
+- EMAIL_PASSWORD ```Sender email's Apps Password(gmail)```
+- SERVICES ```Services name sepearted by comma. Eg: Producer, Consumer```
+- SERVICES_URLS ```Services get URLs seperated by comma. Eg: https//www.consumer.com/get, https//www.producer.com/get```
+- ADMINS_EMAILS ```recipient mail addresses seperated by comma. Eg: utalmighty@gmail.com, utkarsh@infosys.com```
+- PING_INTERVAL_IN_MINUTES ```Interval in minutes```
+- EMAIL_TIME ```Time of day at which mail should be sent```
--- monitor.py
+++ monitor.py
@@ -1,85 +1,85 @@
-import requests
+import os
+import smtplib
 import time
-import schedule
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import smtplib
-import os
 
-sender = os.environ['EMAIL']
-password = os.environ['EMAIL_PASSWORD']
-pingInterval = int(os.environ['PING_INTERVAL_IN_MINUTES'])
-mailTime = os.environ['EMAIL_TIME']
-servicesName = os.environ['SERVICES']
-servicesUrls = os.environ['SERVICES_URLS']
-adminsEmails = os.environ['ADMINS_EMAILS']
+import requests
+import schedule
 
 pings = 0
 name_and_urls = []
-status = [] #(sum_of_pings, failed_pings)
+status = []  # (sum_of_pings, failed_pings)
 admins = []
 
-def initialization() :
+
+def initialization():
+    ''' Read environment varibles '''
     global name_and_urls
     global admins
-    services = servicesName.split(",")
-    urls = servicesUrls.split(",")
-    admins = adminsEmails.split(",")
+    services = os.environ['SERVICES'].split(",")
+    urls = os.environ['SERVICES_URLS'].split(",")
     name_and_urls = []
-    for i in range(len(services)) :
-        name_and_urls.append((services[i].strip(), urls[i].strip()))
-    for i in range(len(admins)) :
-        admins[i] = admins[i].strip()
+    for i, service in enumerate(services):
+        name_and_urls.append((service.strip(), urls[i].strip()))
+    admins = [admin.strip() for admin in os.environ['ADMINS_EMAILS'].split(",")]
+    reset()
 
-def job() :
+
+def job():
+    ''' Periodic request Job '''
     global pings
     global status
     pings += 1
-    for i, item in enumerate(name_and_urls) :
+    for i, item in enumerate(name_and_urls):
         start_time = time.time()
-        resp = requests.get(item[1])
-        if (resp.status_code == 200) :
+        resp = requests.get(item[1]) # No timeout
+        if (resp.status_code == 200):
             status[i][0] += int((time.time()-start_time)*1000)
-        else :
+        else:
             # Increase failure count
-            status[i][1] += 1 
+            status[i][1] += 1
 
-def sendReport() :
-    
+
+def send_report():
+    ''' Email Report '''
     report = "Total Pings: " + str(pings) + "\n"
-    for i, item in enumerate(name_and_urls) :
+    for i, item in enumerate(name_and_urls):
         denominator = pings-status[i][1]
-        averagePing = "Service unavailable!"
-        if (denominator > 0) :
-            averagePing = str(status[i][0]/denominator)+" ms"
-        report += "👉" + item[0] + " 🟢Average Ping Latency: " + averagePing + " 🔴Failed Pings: " + str(status[i][1]) + "\n"
-
-    t = time.localtime()
-    subject = "Report: "+ str(t.tm_mday) + "-" + str(t.tm_mon) + "-" + str(t.tm_year)
-    
+        average_ping = "Service unavailable!"
+        if denominator > 0:
+            average_ping = str(status[i][0]/denominator)+" ms"
+        report += "👉" + item[0] + " 🟢Average Ping Latency: " + \
+            average_ping + " 🔴Failed Pings: " + str(status[i][1]) + "\n"
+    local_time = time.localtime()
+    subject = "Report: " + str(local_time.tm_mday) + "-" + \
+        str(local_time.tm_mon) + "-" + str(local_time.tm_year)
     smtp = smtplib.SMTP('smtp.gmail.com', 587)
     smtp.ehlo()
     smtp.starttls()
-    smtp.login(sender, password)
+    smtp.login(user=os.environ['EMAIL'], password=os.environ['EMAIL_PASSWORD'])
     msg = MIMEMultipart()
     msg['Subject'] = subject
     msg.attach(MIMEText(report))
-    smtp.sendmail(from_addr='Well Wishers Admin', to_addrs=admins, msg=msg.as_string())
+    smtp.sendmail(from_addr='Well Wishers Admin',
+                  to_addrs=admins, msg=msg.as_string())
     smtp.quit()
     reset()
 
-def reset() :
+
+def reset():
+    '''Reset all the values to default'''
     global pings
     global status
     pings = 0
     status = []
-    for i in range(len(name_and_urls)) :
-        status.append([0,0])
+    for i in range(len(name_and_urls)):
+        status.append([0, 0])
 
-if __name__ == "__main__" :
-    reset()
+
+if __name__ == "__main__":
     initialization()
-    schedule.every(pingInterval).minutes.do(job)
-    schedule.every().day.at(mailTime).do(sendReport)
-    while (True) :
+    schedule.every(int(os.environ['PING_INTERVAL_IN_MINUTES'])).minutes.do(job)
+    schedule.every().day.at(os.environ['EMAIL_TIME']).do(send_report)
+    while True:
         schedule.run_pending()
