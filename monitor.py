c6963ef1403f758e77edc83e8649f891ad6f990f
Utkarsh Jaiswal <utalmighty@gmail.com>
2023-07-27 06:54:08 +0000

    Stop monitoring feature

--- monitor.py
+++ monitor.py
@@ -8,18 +8,32 @@ from Job import Job
 app = Flask(__name__)
 obj = Job()
 
+
 @app.route("/start")
 def start():
-    if not obj.is_running :
+    ''' Start Monitoring if not already running '''
+    if not obj.is_running:
         obj.is_running = True
-        while True :
+        while obj.is_running:
             schedule.run_pending()
-    return "Already Running"
+    return "Already Running!"
+
+
+@app.route("/stop")
+def stop():
+    ''' Stop Monitoring if running '''
+    if obj.is_running:
+        obj.is_running = False
+        return "Monitoring Stopped!"
+    return "Monitoring already stopped!"
+
 
 @app.route("/")
 def hello():
+    ''' Say hello '''
     sys_time = time.localtime()
-    return "Hello from Alive!! System Time: "+ str(sys_time.tm_hour) + ":" + str(sys_time.tm_min)
+    return "Hello from Alive!! System Time: " + str(sys_time.tm_hour) + ":" + str(sys_time.tm_min) + " Ping count: " + str(obj.pings)
+
 
 if __name__ == "__main__":
     schedule.every(obj.interval).minutes.do(obj.job)
